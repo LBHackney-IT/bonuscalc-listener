4efbe05d7f71352f43bbca594f637b6fc6570627
Dave Dean <david.dean@hackney.gov.uk>
2021-11-08 18:15:44 +0000

    Message processor factory boilerplate

---
+++ BonusCalcListener/RepairsEventTypes.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BonusCalcListener
+{
+    public static class RepairsEventTypes
+    {
+        public const string WorkOrderCompletedEvent = "WorkOrderCompletedEvent";
+        public const string WorkOrderUpdatedEvent = "WorkOrderUpdatedEvent";
+    }
+}
