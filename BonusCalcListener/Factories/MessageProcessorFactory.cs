9dff48bff17d896fc7874ff44becdc813bfc1cb2
Dave Dean <david.dean@hackney.gov.uk>
2021-11-08 17:48:20 +0000

    Add message processor factory

---
+++ BonusCalcListener/Factories/MessageProcessorFactory.cs
@@ -0,0 +1,20 @@
+using BonusCalcListener.Boundary;
+using BonusCalcListener.UseCase.Interfaces;
+using System;
+
+namespace BonusCalcListener.Factories
+{
+    //Chooses most suitable service for message processing, given the message type
+    public static class MessageProcessorFactory
+    {
+        public static IMessageProcessing CreateMessageProcessor(EntityEventSns evt, IServiceProvider serviceProvider)
+        {
+            if (evt is null) throw new ArgumentNullException(nameof(evt));
+            if (serviceProvider is null) throw new ArgumentNullException(nameof(serviceProvider));
+
+            IMessageProcessing processor = null;
+
+            return processor;
+        }
+    }
+}
