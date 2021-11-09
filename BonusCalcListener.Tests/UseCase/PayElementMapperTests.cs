7cb587b83f5b0e39ce6166de9a20178a205b5a94
Dave Dean <david.dean@hackney.gov.uk>
2021-11-09 18:37:35 +0000

    Pay element mapper + tests

---
+++ BonusCalcListener.Tests/UseCase/PayElementMapperTests.cs
@@ -0,0 +1,86 @@
+using AutoFixture;
+using BonusCalcListener.Boundary;
+using BonusCalcListener.Infrastructure;
+using BonusCalcListener.UseCase;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BonusCalcListener.Tests.UseCase
+{
+    [TestFixture]
+    public class PayElementMapperTests
+    {
+        private PayElementMapper _sut;
+        private readonly Fixture _fixture = new Fixture();
+
+        [Test]
+        public void ShouldThrowWithInvalidArguments_Closedtime()
+        {
+            // Arrange
+            var timesheet = _fixture.Create<Timesheet>();
+            var evtData = _fixture.Build<WorkOrderOperativeSmvData>()
+                .Without(e => e.ClosedTime)
+                .Create();
+
+            // Act + Assert
+            _sut.Invoking(s => s.BuildPayElement(evtData, timesheet))
+                .Should().Throw<ArgumentException>()
+                .WithMessage($"Cannot create a pay element with null closed time, WorkOrder: {evtData.WorkOrderId}, operative: {evtData.OperativeId}");               
+        }
+
+        [Test]
+        public void ShouldThrowWithInvalidArguments_JobPercentage()
+        {
+            // Arrange
+            var timesheet = _fixture.Create<Timesheet>();
+            var evtData = _fixture.Build<WorkOrderOperativeSmvData>()
+                .With(e => e.JobPercentage, -1d)
+                .Create();
+
+            // Act + Assert
+            _sut.Invoking(s => s.BuildPayElement(evtData, timesheet))
+                .Should().Throw<ArgumentException>()
+                .WithMessage($"Cannot have a job percentage of zero or less than zero, WorkOrder: {evtData.WorkOrderId}");
+
+        }
+
+        [Test]
+        public void ShouldCreatePayElementWithValidArguments()
+        {
+            // Arrange
+            var timesheet = _fixture.Create<Timesheet>();
+            var evtData = _fixture.Create<WorkOrderOperativeSmvData>();
+            evtData.ClosedTime = new DateTime(2021, 11, 08, 14, 32, 01);
+
+            // Act
+            var result = _sut.BuildPayElement(evtData, timesheet);
+
+            // Assert
+            result.TimesheetId.Should().Be(timesheet.Id);
+            result.Monday.Should().Be((decimal) (evtData.StandardMinuteValue * evtData.JobPercentage) / 60);
+            result.Tuesday.Should().Be(0);
+            result.Wednesday.Should().Be(0);
+            result.Thursday.Should().Be(0);
+            result.Friday.Should().Be(0);
+            result.Saturday.Should().Be(0);
+            result.Sunday.Should().Be(0);
+            result.Duration.Should().Be((decimal) (evtData.StandardMinuteValue * evtData.JobPercentage) / 60);
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            // Allow circular refs during object generation
+            _fixture.Behaviors.OfType<ThrowingRecursionBehavior>()
+                .ToList()
+                .ForEach(b => _fixture.Behaviors.Remove(b));
+            _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
+            _sut = new PayElementMapper();
+        }
+    }
+}
---
+++ BonusCalcListener/UseCase/PayElementMapper.cs
@@ -0,0 +1,38 @@
+using BonusCalcListener.Boundary;
+using BonusCalcListener.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BonusCalcListener.UseCase
+{
+    public class PayElementMapper : IMapPayElements
+    {
+        const int REACTIVE_REPAIRS_TYPE = 301;
+        public PayElement BuildPayElement(WorkOrderOperativeSmvData eventData, Timesheet operativeTimesheet)
+        {
+            if (!eventData.ClosedTime.HasValue) throw new ArgumentException($"Cannot create a pay element with null closed time, WorkOrder: {eventData.WorkOrderId}, operative: {eventData.OperativeId}");
+
+            if (!eventData.JobPercentage.HasValue || eventData.JobPercentage <= 0) throw new ArgumentException($"Cannot have a job percentage of zero or less than zero, WorkOrder: {eventData.WorkOrderId}");
+
+            var operativeJobSmvHours = (decimal) (eventData.StandardMinuteValue * eventData.JobPercentage ?? 0) / 60;
+
+            return new PayElement
+            {
+                TimesheetId = operativeTimesheet.Id,
+                PayElementTypeId = REACTIVE_REPAIRS_TYPE,
+                Address = eventData.Address,
+                WorkOrder = eventData.WorkOrderId,
+                Duration = operativeJobSmvHours,
+                ReadOnly = true,
+                Monday = eventData.ClosedTime?.DayOfWeek == DayOfWeek.Monday ? operativeJobSmvHours : 0.0m,
+                Tuesday = eventData.ClosedTime?.DayOfWeek == DayOfWeek.Tuesday ? operativeJobSmvHours : 0.0m,
+                Wednesday = eventData.ClosedTime?.DayOfWeek == DayOfWeek.Wednesday ? operativeJobSmvHours : 0.0m,
+                Thursday = eventData.ClosedTime?.DayOfWeek == DayOfWeek.Thursday ? operativeJobSmvHours : 0.0m,
+                Friday = eventData.ClosedTime?.DayOfWeek == DayOfWeek.Friday ? operativeJobSmvHours : 0.0m,
+                Saturday = eventData.ClosedTime?.DayOfWeek == DayOfWeek.Saturday ? operativeJobSmvHours : 0.0m,
+                Sunday = eventData.ClosedTime?.DayOfWeek == DayOfWeek.Sunday ? operativeJobSmvHours : 0.0m,
+            };
+        }
+    }
+}
