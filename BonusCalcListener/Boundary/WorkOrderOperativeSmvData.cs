4919350ee5038eeee75931b3ed2f5b7b0c474a41
Dave Dean <david.dean@hackney.gov.uk>
2021-11-09 22:21:50 +0000

    Rename some elements + csproj

---
+++ BonusCalcListener/Boundary/WorkOrderOperativeSmvData.cs
@@ -0,0 +1,18 @@
+using BonusCalcListener.Infrastructure;
+using System;
+using System.Collections.Generic;
+
+namespace BonusCalcListener.Boundary
+{
+    public class WorkOrderOperativeSmvData
+    {
+        public string WorkOrderId { get; set; }
+        public string Address { get; set; }
+
+        // The SMV for the whole job
+        public int StandardMinuteValue { get; set; }
+        public string OperativeId { get; set; }
+        public double? JobPercentage { get; set; }
+        public DateTime? ClosedTime { get; set; }
+    }
+}
