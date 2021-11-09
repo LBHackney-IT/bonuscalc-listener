f3ed9a8f6a6e6a2881291d3f6d9d26d35e9bc02e
Dave Dean <david.dean@hackney.gov.uk>
2021-11-09 21:45:12 +0000

    pay element gateway + tests

--- BonusCalcListener/Gateway/PayElementGateway.cs
+++ BonusCalcListener/Gateway/PayElementGateway.cs
@@ -1,8 +1,5 @@
-using BonusCalcListener.Boundary;
 using BonusCalcListener.Infrastructure;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
