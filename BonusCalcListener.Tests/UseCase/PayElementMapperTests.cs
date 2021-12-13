7bec9af71520cc62f12cd61d795a70da42e5f166
Andrew White <andrew.white@unboxed.co>
2021-12-13 17:50:18 +0000

    Map overtime pay elements

--- BonusCalcListener.Tests/UseCase/PayElementMapperTests.cs
+++ BonusCalcListener.Tests/UseCase/PayElementMapperTests.cs
@@ -56,6 +56,7 @@ namespace BonusCalcListener.Tests.UseCase
             evtData.JobPercentage = 50;
             evtData.ClosedTime = new DateTime(2021, 11, 08, 14, 32, 01);
             evtData.IsOutOfHours = false;
+            evtData.IsOvertime = false;
             evtData.OperativeCost = 0;
 
             // Act
@@ -88,6 +89,7 @@ namespace BonusCalcListener.Tests.UseCase
             evtData.JobPercentage = 50;
             evtData.ClosedTime = new DateTime(2021, 11, 08, 14, 32, 01);
             evtData.IsOutOfHours = false;
+            evtData.IsOvertime = false;
             evtData.OperativeCost = 0;
 
             // Act
@@ -120,6 +122,7 @@ namespace BonusCalcListener.Tests.UseCase
             evtData.JobPercentage = 50;
             evtData.ClosedTime = new DateTime(2021, 11, 08, 14, 32, 01);
             evtData.IsOutOfHours = true;
+            evtData.IsOvertime = false;
             evtData.OperativeCost = 68;
 
             // Act
@@ -152,6 +155,7 @@ namespace BonusCalcListener.Tests.UseCase
             evtData.JobPercentage = 50;
             evtData.ClosedTime = new DateTime(2021, 11, 08, 14, 32, 01);
             evtData.IsOutOfHours = true;
+            evtData.IsOvertime = false;
             evtData.OperativeCost = 68;
 
             // Act
@@ -159,6 +163,72 @@ namespace BonusCalcListener.Tests.UseCase
 
             // Assert
             result.PayElementTypeId.Should().Be(501);
+            result.WorkOrder.Should().Be(evtData.WorkOrderId);
+            result.Address.Should().Be(evtData.Address);
+            result.Comment.Should().Be(evtData.Description);
+            result.ClosedAt.Should().Be(evtData.ClosedTime);
+            result.Monday.Should().Be(0.0m);
+            result.Tuesday.Should().Be(0.0m);
+            result.Wednesday.Should().Be(0.0m);
+            result.Thursday.Should().Be(0.0m);
+            result.Friday.Should().Be(0.0m);
+            result.Saturday.Should().Be(0.0m);
+            result.Sunday.Should().Be(0.0m);
+            result.Duration.Should().Be(0.0m);
+            result.Value.Should().Be(0.0m);
+        }
+
+        [Test]
+        public void ShouldCreateOvertimePayElementWithValidArguments()
+        {
+            // Arrange
+            var evtData = _fixture.Create<WorkOrderOperativeSmvData>();
+            evtData.WorkOrderStatusCode = 50;
+            evtData.StandardMinuteValue = 0;
+            evtData.JobPercentage = 50;
+            evtData.ClosedTime = new DateTime(2021, 11, 08, 14, 32, 01);
+            evtData.IsOutOfHours = false;
+            evtData.IsOvertime = true;
+            evtData.OperativeCost = 0;
+
+            // Act
+            var result = _sut.BuildPayElement(evtData);
+
+            // Assert
+            result.PayElementTypeId.Should().Be(401);
+            result.WorkOrder.Should().Be(evtData.WorkOrderId);
+            result.Address.Should().Be(evtData.Address);
+            result.Comment.Should().Be(evtData.Description);
+            result.ClosedAt.Should().Be(evtData.ClosedTime);
+            result.Monday.Should().Be(0.0m);
+            result.Tuesday.Should().Be(0.0m);
+            result.Wednesday.Should().Be(0.0m);
+            result.Thursday.Should().Be(0.0m);
+            result.Friday.Should().Be(0.0m);
+            result.Saturday.Should().Be(0.0m);
+            result.Sunday.Should().Be(0.0m);
+            result.Duration.Should().Be(0.0m);
+            result.Value.Should().Be(21.6m);
+        }
+
+        [Test]
+        public void ShouldCreateOvertimePayElementWithZeroValue()
+        {
+            // Arrange
+            var evtData = _fixture.Create<WorkOrderOperativeSmvData>();
+            evtData.WorkOrderStatusCode = 1000;
+            evtData.StandardMinuteValue = 0;
+            evtData.JobPercentage = 50;
+            evtData.ClosedTime = new DateTime(2021, 11, 08, 14, 32, 01);
+            evtData.IsOutOfHours = false;
+            evtData.IsOvertime = true;
+            evtData.OperativeCost = 0;
+
+            // Act
+            var result = _sut.BuildPayElement(evtData);
+
+            // Assert
+            result.PayElementTypeId.Should().Be(401);
             result.WorkOrder.Should().Be(evtData.WorkOrderId);
             result.Address.Should().Be(evtData.Address);
             result.Comment.Should().Be(evtData.Description);
