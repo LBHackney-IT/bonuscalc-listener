6092d1b8701ea44512ebeb68ece9efb64ab035c6
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2025-01-24 10:22:44 +0000

    comment out SeedData method

--- BonusCalcListener.Tests/DatabaseTests.cs
+++ BonusCalcListener.Tests/DatabaseTests.cs
@@ -28,7 +28,7 @@ namespace BonusCalcListener.Tests
 
             _transaction = BonusCalcContext.Database.BeginTransaction();
 
-            await SeedData();
+            //await SeedData();
         }
 
         [TearDown]
@@ -38,113 +38,113 @@ namespace BonusCalcListener.Tests
             _transaction.Dispose();
         }
 
-        protected async Task SeedData()
-        {
-            var trade = new Trade
-            {
-                Id = "EL",
-                Description = "Electrician"
-            };
+        //protected async Task SeedData()
+        //{
+        //    var trade = new Trade
+        //    {
+        //        Id = "EL",
+        //        Description = "Electrician"
+        //    };
 
-            var scheme = new Scheme
-            {
-                Id = 1,
-                Type = "SMV",
-                Description = "Reactive",
-                ConversionFactor = 1.0M
-            };
+        //    var scheme = new Scheme
+        //    {
+        //        Id = 1,
+        //        Type = "SMV",
+        //        Description = "Reactive",
+        //        ConversionFactor = 1.0M
+        //    };
 
-            var operative = new Operative
-            {
-                Id = "123456",
-                Name = "An Operative",
-                Trade = trade,
-                Scheme = scheme,
-                Section = "H3007",
-                SalaryBand = 5,
-                FixedBand = false,
-                IsArchived = false,
-                EmailAddress = "test@test.gov.uk"
-            };
+        //    var operative = new Operative
+        //    {
+        //        Id = "123456",
+        //        Name = "An Operative",
+        //        Trade = trade,
+        //        Scheme = scheme,
+        //        Section = "H3007",
+        //        SalaryBand = 5,
+        //        FixedBand = false,
+        //        IsArchived = false,
+        //        EmailAddress = "test@test.gov.uk"
+        //    };
 
-            var bonusPeriod = new BonusPeriod
-            {
-                Id = "2021-08-02",
-                StartAt = new DateTime(2021, 8, 1, 23, 0, 0, DateTimeKind.Utc),
-                Year = 2020,
-                Number = 3,
-                ClosedAt = null,
-                Weeks = new List<Week>()
-                {
-                    new Week
-                    {
-                        Id = "2021-10-04",
-                        StartAt = new DateTime(2021, 10, 3, 23, 0, 0, DateTimeKind.Utc),
-                        Number = 10,
-                        ClosedAt = new DateTime(2021, 10, 11, 16, 0, 0, DateTimeKind.Utc)
-                    },
-                    new Week
-                    {
-                        Id = "2021-10-11",
-                        StartAt = new DateTime(2021, 10, 10, 23, 0, 0, DateTimeKind.Utc),
-                        Number = 11,
-                        ClosedAt = null
-                    },
-                    new Week
-                    {
-                        Id = "2021-10-18",
-                        StartAt = new DateTime(2021, 10, 17, 23, 0, 0, DateTimeKind.Utc),
-                        Number = 12,
-                        ClosedAt = null
-                    },
-                    new Week
-                    {
-                        Id = "2021-10-25",
-                        StartAt = new DateTime(2021, 10, 24, 23, 0, 0, DateTimeKind.Utc),
-                        Number = 13,
-                        ClosedAt = null
-                    }
-                }
-            };
+        //    var bonusPeriod = new BonusPeriod
+        //    {
+        //        Id = "2021-08-02",
+        //        StartAt = new DateTime(2021, 8, 1, 23, 0, 0, DateTimeKind.Utc),
+        //        Year = 2020,
+        //        Number = 3,
+        //        ClosedAt = null,
+        //        Weeks = new List<Week>()
+        //        {
+        //            new Week
+        //            {
+        //                Id = "2021-10-04",
+        //                StartAt = new DateTime(2021, 10, 3, 23, 0, 0, DateTimeKind.Utc),
+        //                Number = 10,
+        //                ClosedAt = new DateTime(2021, 10, 11, 16, 0, 0, DateTimeKind.Utc)
+        //            },
+        //            new Week
+        //            {
+        //                Id = "2021-10-11",
+        //                StartAt = new DateTime(2021, 10, 10, 23, 0, 0, DateTimeKind.Utc),
+        //                Number = 11,
+        //                ClosedAt = null
+        //            },
+        //            new Week
+        //            {
+        //                Id = "2021-10-18",
+        //                StartAt = new DateTime(2021, 10, 17, 23, 0, 0, DateTimeKind.Utc),
+        //                Number = 12,
+        //                ClosedAt = null
+        //            },
+        //            new Week
+        //            {
+        //                Id = "2021-10-25",
+        //                StartAt = new DateTime(2021, 10, 24, 23, 0, 0, DateTimeKind.Utc),
+        //                Number = 13,
+        //                ClosedAt = null
+        //            }
+        //        }
+        //    };
 
-            var timesheets = new List<Timesheet>()
-            {
-                new Timesheet
-                {
-                    Id = "123456/2021-10-04",
-                    OperativeId = "123456",
-                    WeekId = "2021-10-04"
-                },
-                new Timesheet
-                {
-                    Id = "123456/2021-10-11",
-                    OperativeId = "123456",
-                    WeekId = "2021-10-11"
-                },
-                new Timesheet
-                {
-                    Id = "123456/2021-10-18",
-                    OperativeId = "123456",
-                    WeekId = "2021-10-18"
-                },
-                new Timesheet
-                {
-                    Id = "123456/2021-10-25",
-                    OperativeId = "123456",
-                    WeekId = "2021-10-25"
-                },
-            };
+        //    var timesheets = new List<Timesheet>()
+        //    {
+        //        new Timesheet
+        //        {
+        //            Id = "123456/2021-10-04",
+        //            OperativeId = "123456",
+        //            WeekId = "2021-10-04"
+        //        },
+        //        new Timesheet
+        //        {
+        //            Id = "123456/2021-10-11",
+        //            OperativeId = "123456",
+        //            WeekId = "2021-10-11"
+        //        },
+        //        new Timesheet
+        //        {
+        //            Id = "123456/2021-10-18",
+        //            OperativeId = "123456",
+        //            WeekId = "2021-10-18"
+        //        },
+        //        new Timesheet
+        //        {
+        //            Id = "123456/2021-10-25",
+        //            OperativeId = "123456",
+        //            WeekId = "2021-10-25"
+        //        },
+        //    };
 
-            // Empty trades table for tests
-            // BonusCalcContext.Trades.RemoveRange(BonusCalcContext.Trades);
-            // await BonusCalcContext.Trades.AddAsync(trade).ConfigureAwait(false);
+        //    // Empty trades table for tests
+        //    // BonusCalcContext.Trades.RemoveRange(BonusCalcContext.Trades);
+        //    // await BonusCalcContext.Trades.AddAsync(trade).ConfigureAwait(false);
 
-            BonusCalcContext.Schemes.Add(scheme);
-            BonusCalcContext.Operatives.Add(operative);
-            BonusCalcContext.BonusPeriods.Add(bonusPeriod);
-            BonusCalcContext.Timesheets.AddRange(timesheets);
+        //    BonusCalcContext.Schemes.Add(scheme);
+        //    BonusCalcContext.Operatives.Add(operative);
+        //    BonusCalcContext.BonusPeriods.Add(bonusPeriod);
+        //    BonusCalcContext.Timesheets.AddRange(timesheets);
 
-            await BonusCalcContext.SaveChangesAsync();
-        }
+        //    await BonusCalcContext.SaveChangesAsync();
+        //}
     }
 }
