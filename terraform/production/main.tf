da4167840e81f061108a01e748c71fa48e55a85d
C̵͉͋̔͞ā̤̓̍͘l̙͖̑̾ͣl̙͖̑̾ͣư̡͕̭̇ḿ̬̏ͤͅ M͉̅ͮ͒ͤā̤̓̍͘c͕͗ͤ̕̕p̞̈͑̚͞ḣ̖̻͛̓ẹ̿͋̒̕r̴̨̦͕̝s̠҉͍͊ͅo̯̱̊͊͢ṇ̤͛̒̍ <callum.macpherson@hackney.gov.uk>
2026-01-28 09:57:51 +0000

    Parameter already exists

--- terraform/production/main.tf
+++ terraform/production/main.tf
@@ -51,11 +51,11 @@ resource "aws_sns_topic" "repairs" {
   kms_master_key_id           = "alias/aws/sns"
 }
 
-resource "aws_ssm_parameter" "repairs_sns_arn" {
-  name  = "/sns-topic/production/repairs/arn"
-  type  = "String"
-  value = aws_sns_topic.repairs.arn
-}
+# resource "aws_ssm_parameter" "repairs_sns_arn" {
+#   name  = "/sns-topic/production/repairs/arn"
+#   type  = "String"
+#   value = aws_sns_topic.repairs.arn
+# }
 
 data "aws_ssm_parameter" "repairs_sns_topic_arn" {
   name = "/sns-topic/production/repairs/arn"
